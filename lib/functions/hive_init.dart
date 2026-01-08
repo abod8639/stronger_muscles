13039844642b4d0497de68ef774487ed9836a625
abot8639 <abot8639@gmail.com>
2026-01-08 18:11:24 +0200

    create hiveInit

---
+++ lib/functions/hive_init.dart
@@ -0,0 +1,26 @@
+import 'package:hive/hive.dart';
+import 'package:stronger_muscles/data/models/address_model.dart';
+import 'package:stronger_muscles/data/models/cart_item_model.dart';
+import 'package:stronger_muscles/data/models/category_model.dart';
+import 'package:stronger_muscles/data/models/order_model.dart';
+import 'package:stronger_muscles/data/models/product_model.dart';
+import 'package:stronger_muscles/data/models/user_model.dart';
+
+Future<void> hiveInit() async {
+    // Register Adapters
+  Hive.registerAdapter(CartItemModelAdapter());
+  Hive.registerAdapter(OrderModelAdapter());
+  Hive.registerAdapter(OrderItemModelAdapter());
+  Hive.registerAdapter(AddressModelAdapter());
+  Hive.registerAdapter(UserModelAdapter());
+  Hive.registerAdapter(ProductModelAdapter());
+  Hive.registerAdapter(CategoryModelAdapter());
+  
+  // Open Boxes
+  await Hive.openBox<CartItemModel>('cart');
+  await Hive.openBox<String>('wishlist');
+  await Hive.openBox<CategoryModel>('categories');
+  await Hive.openBox('settings'); // For theme and language settings
+  await Hive.openBox('auth_box'); // Open the box for auth tokens
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:stronger_muscles/data/models/cart_item_model.dart';
-import 'package:stronger_muscles/data/models/product_model.dart';
-import 'package:stronger_muscles/data/models/user_model.dart';
 import 'package:stronger_muscles/firebase_options.dart';
+import 'package:stronger_muscles/functions/hive_init.dart';
 import 'package:stronger_muscles/functions/init_controllers_app.dart';
 import 'package:stronger_muscles/core/constants/app_theme.dart';
 import 'package:stronger_muscles/routes/routes.dart';
-import 'package:stronger_muscles/data/models/category_model.dart';
-import 'package:stronger_muscles/data/models/order_model.dart';
-import 'package:stronger_muscles/data/models/address_model.dart';
 import 'package:stronger_muscles/presentation/bindings/theme_controller.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:stronger_muscles/presentation/bindings/language_controller.dart';
@@ -20,34 +15,18 @@ import 'package:stronger_muscles/l10n/generated/app_localizations.dart';
 import 'package:stronger_muscles/presentation/widgets/internet_connection_banner.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env"); // Load the .env file
   if (Firebase.apps.isEmpty) {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
   await Hive.initFlutter();
-  
-  // Register Adapters
-  Hive.registerAdapter(CartItemModelAdapter());
-  Hive.registerAdapter(OrderModelAdapter());
-  Hive.registerAdapter(OrderItemModelAdapter());
-  Hive.registerAdapter(AddressModelAdapter());
-  Hive.registerAdapter(UserModelAdapter());
-  Hive.registerAdapter(ProductModelAdapter());
-  Hive.registerAdapter(CategoryModelAdapter());
-  
-  // Open Boxes
-  await Hive.openBox<CartItemModel>('cart');
-  await Hive.openBox<String>('wishlist');
-  await Hive.openBox<CategoryModel>('categories');
-  await Hive.openBox('settings'); // For theme and language settings
-  await Hive.openBox('auth_box'); // Open the box for auth tokens
-  
-  runApp(const MyApp());
-  
-}
 
+  hiveInit();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -58,33 +37,35 @@ class MyApp extends StatelessWidget {
     final themeController = Get.put(ThemeController());
     final languageController = Get.put(LanguageController());
 
-    return Obx(() => GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: themeController.themeMode,
-      initialBinding: initControllersApp(),
-      initialRoute: AppRoutes.main,
-      getPages: AppPages.routes,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      locale: languageController.currentLocale.value,
-      builder: (context, child) => Stack(
-        children: [
-          child!,
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: InternetConnectionBanner(),
-          ),
+    return Obx(
+      () => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: themeController.themeMode,
+        initialBinding: initControllersApp(),
+        initialRoute: AppRoutes.main,
+        getPages: AppPages.routes,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
         ],
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: languageController.currentLocale.value,
+        builder: (context, child) => Stack(
+          children: [
+            child!,
+            const Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: InternetConnectionBanner(),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
