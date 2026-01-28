aaf384de5d0055e7379cb0b65942efeb2efdda5b
abot8639 <abot8639@gmail.com>
2026-01-28 06:00:41 +0200

    fix LoginPromptCard

--- lib/presentation/pages/profile/widgets/login_prompt_card.dart
+++ lib/presentation/pages/profile/widgets/login_prompt_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:stronger_muscles/core/constants/app_colors.dart';
-import 'package:stronger_muscles/functions/singin_chek.dart';
+import 'package:stronger_muscles/functions/app_guard.dart';
 import 'package:stronger_muscles/presentation/controllers/profile_controller.dart';
 import 'package:stronger_muscles/presentation/pages/auth/auth_view.dart';
 import 'package:stronger_muscles/presentation/pages/profile/widgets/account_settings_list.dart';
