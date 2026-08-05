67526dcd7c22f2787802475a2dfd540cc9dbc6ba
abot8639 <abot8639@gmail.com>
2026-08-05 06:56:11 +0300

    chore: update dependencies and optimize order controller loading state for profile page.

--- lib/features/home/presentation/controllers/main_controller.g.dart
+++ lib/features/home/presentation/controllers/main_controller.g.dart
@@ -6,12 +6,11 @@ part of 'main_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$mainControllerHash() => r'd02d378e8fd4f91cac1f898101d47e00301519bf';
+String _$mainControllerHash() => r'b1c3e55a954acea013596f9f0fddd432b275e12a';
 
 /// See also [MainController].
 @ProviderFor(MainController)
-final mainControllerProvider =
-    AutoDisposeNotifierProvider<MainController, int>.internal(
+final mainControllerProvider = NotifierProvider<MainController, int>.internal(
   MainController.new,
   name: r'mainControllerProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +20,6 @@ final mainControllerProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$MainController = AutoDisposeNotifier<int>;
+typedef _$MainController = Notifier<int>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
