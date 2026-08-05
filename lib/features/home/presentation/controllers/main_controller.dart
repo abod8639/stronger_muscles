09087e00bece06cf306a5e2e48fed3bc4dffb7dc
abot8639 <abot8639@gmail.com>
2026-08-05 06:56:06 +0300

    refactor: decouple profile data from ProfileController by migrating stats and operations to specialized feature controllers

--- lib/features/home/presentation/controllers/main_controller.dart
+++ lib/features/home/presentation/controllers/main_controller.dart
@@ -2,7 +2,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'main_controller.g.dart';
 
-@riverpod
+@Riverpod(keepAlive: true)
 class MainController extends _$MainController {
   @override
   int build() => 0;
