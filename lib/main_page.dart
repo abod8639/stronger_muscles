a2da35ad6166ac21e8420fe01adac55af661b072
abot8639 <abot8639@gmail.com>
2026-09-22 04:27:22 +0300

    refactor ProductRepository to a standard provider, decouple cart content widget, and add unit tests

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,35 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:stronger_muscles/features/home/presentation/controllers/main_controller.dart';
-import 'package:stronger_muscles/features/cart/presentation/pages/cart_view.dart';
-import 'package:stronger_muscles/features/home/presentation/pages/home_view.dart';
-import 'package:stronger_muscles/features/profile/presentation/pages/profile_page.dart';
-import 'package:stronger_muscles/features/wishlist/presentation/pages/wishlist_view.dart';
+import 'package:go_router/go_router.dart';
 import 'package:stronger_muscles/core/utils/components/my_bottom_navigation_bar.dart';
 
-class MainPage extends ConsumerWidget {
-  const MainPage({super.key});
+class MainPage extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const MainPage({super.key, required this.navigationShell});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final tabIndex = ref.watch(mainControllerProvider);
-
-    final pages = <Widget>[
-      const HomeView(),
-      const WishlistView(),
-      const CartView(),
-      const ProfilePage(),
-    ];
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: IndexedStack(index: tabIndex, children: pages),
-          ),
-        ],
+      body: navigationShell,
+      bottomNavigationBar: MyBottomNavigationBar(
+        navigationShell: navigationShell,
       ),
-      bottomNavigationBar: const MyBottomNavigationBar(),
     );
   }
 }
