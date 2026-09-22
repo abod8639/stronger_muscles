84870264af5dd25131b04e6474220955130ac7a8
abot8639 <abot8639@gmail.com>
2026-09-22 04:30:40 +0300

    refactor: implement StatefulShellRoute for navigation and add unit tests

--- lib/features/profile/presentation/controllers/address_controller.g.dart
+++ lib/features/profile/presentation/controllers/address_controller.g.dart
@@ -6,7 +6,41 @@ part of 'address_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$addressControllerHash() => r'f703bad0d080d7e821caf29b1c0437cb70beef51';
+String _$addressFormLoadingHash() =>
+    r'47175010d6c45a2d289cb3f95b443e3131962b7d';
+
+/// See also [AddressFormLoading].
+@ProviderFor(AddressFormLoading)
+final addressFormLoadingProvider =
+    AutoDisposeNotifierProvider<AddressFormLoading, bool>.internal(
+  AddressFormLoading.new,
+  name: r'addressFormLoadingProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addressFormLoadingHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AddressFormLoading = AutoDisposeNotifier<bool>;
+String _$addressFormSelectedLabelHash() =>
+    r'119852c211d92ccff3c7db096b71331f17397a47';
+
+/// See also [AddressFormSelectedLabel].
+@ProviderFor(AddressFormSelectedLabel)
+final addressFormSelectedLabelProvider =
+    AutoDisposeNotifierProvider<AddressFormSelectedLabel, String>.internal(
+  AddressFormSelectedLabel.new,
+  name: r'addressFormSelectedLabelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addressFormSelectedLabelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AddressFormSelectedLabel = AutoDisposeNotifier<String>;
+String _$addressControllerHash() => r'cbae9a1cb4940339709a436ab6475b6747fb6cf0';
 
 /// See also [AddressController].
 @ProviderFor(AddressController)
