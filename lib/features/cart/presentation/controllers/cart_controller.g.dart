6a1acae8470e3449eb4164e52664a2c7cda44325
abot8639 <abot8639@gmail.com>
2026-09-22 04:27:52 +0300

    refactor: update Riverpod controllers and add unit tests for features and core services

---
+++ lib/features/cart/presentation/controllers/cart_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$cartControllerHash() => r'08c25bef01b33a12fdd84f0199e475ad63b8feb5';
+
+/// See also [CartController].
+@ProviderFor(CartController)
+final cartControllerProvider = AutoDisposeAsyncNotifierProvider<CartController,
+    List<CartItemModel>>.internal(
+  CartController.new,
+  name: r'cartControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$cartControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CartController = AutoDisposeAsyncNotifier<List<CartItemModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
