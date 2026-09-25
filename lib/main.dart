ae3d42ab6a072a2eb518c6f88d8aef0288690cb2
abot8639 <abot8639@gmail.com>
2026-09-25 07:52:34 +0300

    feat: add localization keys and strings for checkout and order features

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends ConsumerWidget {
             bottom: 80,
             left: 0,
             right: 0,
-            child: InternetConnectionBanner(title: "No internet connection"),
+            child: InternetConnectionBanner(),
           ),
         ],
       ),
